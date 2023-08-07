21ed6a40c33dd764dff8b9957030fc586f868ba4
FeMerrlo <111811293+FeMerrlo@users.noreply.github.com>
2023-08-07 19:03:00 -0300

    Atualização Projeto
    BancoNF

---
+++ BancoNF/src/main/java/Banco/ContaPoupança.java
@@ -0,0 +1,22 @@
+package Banco;
+
+public class ContaPoupança extends Conta{
+    private double rentabilidade;
+
+    public double getRentabilidade() {
+        return rentabilidade;
+    }
+
+    public void setRentabilidade(double rentabilidade) {
+        this.rentabilidade = rentabilidade;
+    }
+
+    public ContaPoupança() {
+    }
+
+    public ContaPoupança(String agencia,String numero, double rentabilidade) {
+        super(numero, agencia);
+
+        this.rentabilidade = rentabilidade;
+    }
+}
---
+++ BancoNF/src/main/java/Banco/ContaSalario.java
@@ -0,0 +1,11 @@
+package Banco;
+
+public class ContaSalario extends Conta{
+    public ContaSalario() {
+
+    }
+
+    public ContaSalario(String agencia, String numero) {
+        super(agencia, numero);
+    }
+}
